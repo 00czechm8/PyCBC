e36da682c18aa0819060bc3312587a7425770162
Mike Reynolds <mwreynol@umich.edu>
2025-06-13 11:05:04 -0400

    removed pycbc_env

---
+++ Experiment/Test_script.py
@@ -0,0 +1,112 @@
+import Experiment.CBC_lib as CBC_lib
+import numpy as np
+import os
+import multiprocessing
+from daqhats import mcc152
+import time
+
+
+def main():
+
+    # Start the backbone object
+    cbc = CBC_lib.Backbone()
+
+    # Start connection with hats channels
+    cbc.start_hats()
+
+    # Create values in class object
+    cbc.filename = "Simple_beam_backbone.txt"
+    cbc.kp = 0.02
+    cbc.kd = 0.05
+    cbc.pause_event = multiprocessing.Event()
+    cbc.pause_event.set() # Pause process when pause_spin_up.clear() is called but runs when pause_spin_up.set()
+    cbc.stop_event = stop_spin_up = multiprocessing.Event() # to Stop process call stop_spin_up.set() (breaks out of the while loop)
+    cbc.output_channel
+    cbc.read_channel
+    cbc.fs
+    cbc.F_spin_up
+    cbc.omega_spin_up
+
+    os.sched_setaffinity(0, {0})
+    # Txt file name for saved data at the end
+    filename = "Simple_beam_backbone.txt"
+
+    # Important Constants
+    omega_init = 0
+    F_start = 0
+    h = 0
+    m = 0
+    fs = 0
+    kp = 0.02
+    kd = 0.05
+    q_tol = 0
+    e_tol = 0
+    n_max = 0
+    Ts = 1 / fs
+    duration = 60 # trial duration
+
+    freq_list = []
+    amp_list = []
+    forcing_list = []
+    omega = [omega_init, omega_init, omega_init * 3]
+    X_n = np.zeros((m + 1, 2, n_max))
+    X_star = np.zeros((m + 1, 2))
+
+    n_counter = 0
+
+    
+    F_spin_up = multiprocessing.Value("d", F_start)
+    omega_spin_up = multiprocessing.Value("d", omega_init)
+    spin_up_process = multiprocessing.Process(target=cbc.spin_up, args=(F_spin_up.value, omega_spin_up.value, cbc.pause_event, cbc.stop_event, cbc.dac, cbc.output_channel))
+    spin_up_process.start()
+
+    while n_counter < n_max:
+        F = F_start + n_counter * h
+        q_omega = [np.inf, np.inf, np.inf]
+        val_dif = np.inf
+        e_u = np.inf
+        e_counter = 1
+        q_counter = 1
+
+        while abs(val_dif) > q_tol and q_counter <= 150:
+            while e_u > e_tol and e_counter <= 100:
+                if e_counter == 1:
+                    X_star[1, 0] = 0
+                    X_star[1, 1] = F
+
+                x_star_func = cbc.get_traj(X_star, omega[1])
+                x_dot_star_func = cbc.get_traj_derivative(X_star, omega[1])
+
+                # RUN SYSTEM HERE
+                cbc.pause_spin_up()
+                signal, F_act, time = cbc.run_system(F, omega[1], x_star_func, x_dot_star_func, duration) 
+                cbc.resume_spin_up()
+                time.sleep(2)
+                
+                seg_signal, F_seg, wl = cbc.segment_signal(signal, F_act, fs)
+                Four_coeffs = cbc.get_four_coeffs(seg_signal, m, omega[1], fs)
+                e_u = np.linalg.norm(Four_coeffs[2:] - X_star[2:]) + np.linalg.norm(Four_coeffs[0] - X_star[0])
+                if e_u > e_tol:
+                    X_star[2:] = Four_coeffs[2:]
+                    X_star[0] = Four_coeffs[0]
+                e_counter += 1
+                
+            # forcing = F_act * np.cos(omega[1] * tspan[seg_start:end_idx])
+            q_omega[1] = cbc.compute_phase_difference(F_seg, seg_signal, fs, wl['seconds'])
+
+            if abs(q_omega[1]) <= q_tol:
+                A, freq = cbc.get_backbone_point(seg_signal, fs)
+                amp_list.append(A)
+                freq_list.append(freq)
+                forcing_list.append(cbc.get_amplitude(F_seg))
+
+            omega = cbc.bisection_point(q_omega, omega, q_counter)
+
+            q_counter += 1
+        n_counter += 1
+    cbc.dac.a_out_write(0, 0)
+    cbc.dac.a_out_write(1, 0)
+    cbc.save_to_txt(filename, amp_list, freq_list)
+
+if __name__ == '__main__':
+    main()
---
+++ Experiment/UnitTests_CBC_lib.py
@@ -0,0 +1,73 @@
+import unittest
+import numpy as np
+from Experiment.CBC_lib import Backbone
+
+class TestCBCLib(unittest.TestCase):
+
+    def setUp(self):
+        self.fs = 1000  # Hz
+        self.omega = 2 * np.pi * 5  # 5 Hz
+        self.t = np.linspace(0, 1, self.fs, endpoint=False)
+        self.signal = np.sin(self.omega * self.t)
+        self.cbc = Backbone()
+        self.cbc.fs = self.fs
+
+    def test_get_traj(self):
+        coeffs = np.zeros((3, 2))
+        coeffs[1, 0] = 1  # cos(omega t)
+        traj = self.cbc.get_traj(coeffs, self.omega)
+        result = traj(self.t)
+        np.testing.assert_allclose(result, np.cos(self.omega * self.t), atol=1e-2)
+
+    def test_get_traj_derivative(self):
+        coeffs = np.zeros((3, 2))
+        coeffs[1, 0] = 1  # cos(omega t)
+        dtraj = self.cbc.get_traj_derivative(coeffs, self.omega)
+        result = dtraj(self.t)
+        expected = -self.omega * np.sin(self.omega * self.t)
+        np.testing.assert_allclose(result, expected, atol=1e-2)
+
+    def test_get_four_coeffs(self):
+        m = 3
+        coeffs = self.cbc.get_four_coeffs(self.signal, m, self.omega, self.fs)
+        self.assertEqual(coeffs.shape, (m + 1, 2))
+        # For sin(ωt), B1 ≈ -1, A1 ≈ 0
+        self.assertAlmostEqual(coeffs[1, 1], -1.0, places=1)
+        self.assertAlmostEqual(coeffs[1, 0], 0.0, places=1)
+
+    def test_get_amplitude(self):
+        amp = self.cbc.get_amplitude(self.signal)
+        self.assertAlmostEqual(amp, 1.0, places=1)
+
+    def test_estimate_wavelength(self):
+        wl = self.cbc.estimate_wavelength(self.signal, self.fs)
+        expected_period = 1 / 5  # since freq = 5 Hz
+        self.assertAlmostEqual(wl['seconds'], expected_period, places=2)
+
+    def test_compute_phase_difference(self):
+        shifted_signal = np.sin(self.omega * self.t + np.pi / 4)
+        phase_diff = self.cbc.compute_phase_difference(self.signal, shifted_signal)
+        # Allow for wrap-around
+        self.assertTrue(np.isclose(abs(phase_diff), np.pi / 4, atol=0.2))
+
+    def test_estimate_dominant_freq(self):
+        freq = self.cbc.estimate_dominant_freq(self.signal)
+        self.assertAlmostEqual(freq, 5.0, places=0)
+
+    def test_segment_signal(self):
+        # Use a longer signal to ensure enough samples
+        long_signal = np.tile(self.signal, 40)  # 40 seconds
+        long_forcing = np.tile(np.cos(self.omega * self.t), 40)
+        seg_signal, seg_forcing, wl = self.cbc.segment_signal(long_signal, long_forcing, self.fs)
+        self.assertEqual(len(seg_signal), len(seg_forcing))
+        self.assertGreater(len(seg_signal), 0)
+        self.assertIn('samples', wl)
+        self.assertIn('seconds', wl)
+
+    def test_get_backbone_point(self):
+        amp, freq = self.cbc.get_backbone_point(self.signal)
+        self.assertAlmostEqual(amp, 1.0, places=1)
+        self.assertAlmostEqual(freq, 5.0, places=0)
+
+if __name__ == '__main__':
+    unittest.main()
